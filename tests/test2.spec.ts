814b54cb4da49f2a40b20cd8ba32cdf83e9a3392
Prateek Sethi <sethiprateek23a@gmail.com>
2026-03-28 13:03:07 +0530

    first commit

--- tests/test2.spec.ts
+++ tests/test2.spec.ts
@@ -1,5 +1,5 @@
 import {test, expect} from "@playwright/test";
 
-test("Test1", async({page})=>{
-    console.log("Hello Test1");
+test("Test2", async({page})=>{
+    console.log("Hello Test2");
 })
